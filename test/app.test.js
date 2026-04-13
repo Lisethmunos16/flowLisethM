444fbd42ae2786299791e6974e936b1ce2383772
Liseth Munoz <lisethmunos234@gmail.com>
2026-04-13 09:22:53 -0500

    se modifico el app.j y el app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,36 @@
+import { saludar, despedir, estadosistema , sumar } from "../src/app.js"
+
+function ejecutarpruebas() {
+    let pasadas = 0;
+    let fallidas = 0;
+
+    const r1 = saludar("Raider")
+    if (r1.includes("Raider")) {
+        console.log("test 1 pasado: funcion saludar en codeRaider es correcta");
+        pasadas++;
+    } else {
+        console.log("test 1 fallido: ", r1);
+        fallidas++;
+    }
+
+    const actualEstado = estadosistema();
+    if (actualEstado.estado === "activo") {
+        console.log("test 2: pasado: estadosistema() funciona en coderider");
+        pasadas++;
+    } else {
+        console.log("test 2 fallido: ", estado, "en codeRider");
+        fallidas++;
+    }
+    console.log("\nResultados: " + pasadas + " pasadas, " + fallidas + "fallidas");
+ 
+    if (fallidas > 0) process.exit(1); "fallidas"
+    const resultadoSuma = sumar(2, 3);
+    if (resultadoSuma === 5) {
+        console.log("test 3: pasado: sumar(2, 3) devuelve 5 en codeRider");
+        pasadas++;
+    } else {
+        console.log("test 3 fallido: sumar(2, 3) devuelve ", resultadoSuma, "en codeRider");
+        fallidas++;
+    }
+}
+ejecutarpruebas();
