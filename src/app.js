1f5eb8dcc2bcdf9632e2b057bce88d8f1d8e1bfc
Liseth Munoz <lisethmunos234@gmail.com>
2026-04-13 09:04:41 -0500

    Corrección de mensajes en app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+ export function saludar(nombre){
+ return "hola" + nombre + ", bienvenido a aplicacion de LisethM!";
+ }
+
+ export function despedir (nombre){
+ return "hasta pronto, " + nombre + "!lisethM";
+ }
+
+ export function estadoSistema() {
+ return {
+ estado : "activo"
+ vercion : "1.0.0"
+ menssage : "sistema funcionando correctamente"
+};
+}
+}
+
